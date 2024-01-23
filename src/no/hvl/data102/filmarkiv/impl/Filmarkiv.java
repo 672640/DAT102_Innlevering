3a7f43e55a18958ed7a42bc15624ffeb57df734b
PotatoKing147 <joachimoen02@gmail.com>
2024-01-23 11:28:12 +0100

    AHHHHH

--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,7 +1,42 @@
 package no.hvl.data102.filmarkiv.impl;
-public class Filmarkiv {
+public class Filmarkiv implements no.hvl.data102.filmarkiv.adt.FilmarkivADT {
 	String[] arkivFilm;
 	public Filmarkiv(int talFilmar) {
 		String[] arkivFilm = new String[talFilmar];
+	}
+	@Override
+	public Film finnFilm(int nr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void leggTilFilm(Film nyFilm) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean slettFilm(int filmnr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Film[] soekTittel(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Film[] soekProdusent(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int antall(Sjanger sjanger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int antall() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
