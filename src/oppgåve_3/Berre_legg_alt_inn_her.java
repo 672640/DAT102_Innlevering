56a83eba2d44ef3041d888d5765494970d94e96d
PotatoKing147 <joachimoen02@gmail.com>
2024-01-23 13:46:23 +0100

    Update Berre_legg_alt_inn_her.java

--- src/oppgåve_3/Berre_legg_alt_inn_her.java
+++ src/oppgåve_3/Berre_legg_alt_inn_her.java
@@ -8,5 +8,18 @@ public class Berre_legg_alt_inn_her {
  * t(n) orden f(n) viss c og N eksisterer + positive, slik at t(x) <= cf(n) for alle n>=N, t(n) ikkje negativt.
  * Stor O-notasjon: t(n) = O(f(n)), t(n) vekstfunksjonen.
  * Når n >= N, grafen t(n) ligge under grafen til cf(n).
+ * 
+ * Ignorerer konstantar, så 5n -> O(n).
+ * O(1) < O(log(n)) < O(n) < O(nlogn) < O(n^2) < O(2^n) < O(n!)
+ * Big. https://www.bigocheatsheet.com/
+ * 
+ */
+	
+/*
+ * 3a)
+ * i: 4n^2 + 50n - 10 = 
+ * ii:
+ * iii:
+ * iv:
  */
 }
