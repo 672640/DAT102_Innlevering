12041986828c87543474e10ed807f00122d7af04
PotatoKing147 <joachimoen02@gmail.com>
2024-01-23 12:45:14 +0100

    å

---
+++ src/oppgåve_3/Berre_legg_alt_inn_her.java
@@ -0,0 +1,12 @@
+package oppgåve_3;
+
+public class Berre_legg_alt_inn_her {
+/*
+ * n positive heiltal, størrelsen på problemet.
+ * Ex. For sortering, problemet vere antal element som skal sorterast.
+ * t() avhengig av problemstørrelsen.
+ * t(n) orden f(n) viss c og N eksisterer + positive, slik at t(x) <= cf(n) for alle n>=N, t(n) ikkje negativt.
+ * Stor O-notasjon: t(n) = O(f(n)), t(n) vekstfunksjonen.
+ * Når n >= N, grafen t(n) ligge under grafen til cf(n).
+ */
+}
