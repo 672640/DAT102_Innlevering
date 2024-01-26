75742036c48a5ba9d3728a4149a7d2e88b268aa0
PotatoKing147 <joachimoen02@gmail.com>
2024-01-26 17:29:20 +0100

    AHHHHHHHHH

--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -39,9 +39,9 @@ public class Filmarkiv implements FilmarkivADT {
 				Film film = this.arkivFilm[i];
 			this.arkivFilm[i] = this.arkivFilm[antall -1];
 		this.arkivFilm[i] = film;
-		this.arkivFilm[antall-1]=null;
-		antall--;
-		return true;
+			this.arkivFilm[antall-1]=null;
+				antall--;
+			return true;
 			}
 		}
 		
@@ -49,22 +49,48 @@ public class Filmarkiv implements FilmarkivADT {
 	}
 	@Override
 	public Film[] soekTittel(String delstreng) {
-		// TODO Auto-generated method stub
+		Film[] arkivFilm2 = new Film[this.arkivFilm.length * 2];
+		for(int i = 0; i < this.arkivFilm.length; i++) {
+			if(this.arkivFilm[i].getTittel().contentEquals(delstreng)) {
+				for(int j = 0; j < arkivFilm2.length; j++) {
+					arkivFilm2 = new Film[j];
+				}
+				return arkivFilm2;
+			}
+		}
 		return null;
 	}
 	@Override
 	public Film[] soekProdusent(String delstreng) {
-		
+		Film[] arkivFilm2 = new Film[this.arkivFilm.length * 2];
+		for(int i = 0; i < this.arkivFilm.length; i++) {
+			if(this.arkivFilm[i].getProdusent().contentEquals(delstreng)) {
+				for(int j = 0; j < arkivFilm2.length; j++) {
+					arkivFilm2 = new Film[j];
+					antall++;
+				}
+				return arkivFilm2;
+			}
+		}
 		return null;
 	}
 	@Override
 	public int antall(Sjanger sjanger) {
-		// TODO Auto-generated method stub
+		int antall = 0;
+		for(int i = 0; i < this.arkivFilm.length; i++) {
+			if(this.arkivFilm[i].getSjanger().equals(sjanger) && antall > 0) {
+				for(Sjanger sjanger1: Sjanger.values()) {
+					
+				}
+				return antall;
+			} else {
+				antall++;
+			}
+		}
 		return 0;
 	}
 	@Override
 	public int antall() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.arkivFilm.length;
 	}
 }
