5852709113a5e55798dcbfbded0b8aceb1be4338
672640 <672640@stud.hvl.no>
2024-01-27 14:45:09 +0100

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Obligatorisk1_Oppgåve {
+	requires junit;
 }
--- src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -1,5 +1,14 @@
 package no.hvl.data102.filmarkiv.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class FilmarkivTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
