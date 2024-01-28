fc9f467ae1b6a24c71599b2497ef36dac5b3ad92
672640 <672640@stud.hvl.no>
2024-01-28 16:45:17 +0100

    Jobbar med oppg2

---
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
@@ -0,0 +1,9 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+public class Filmarkiv2 implements FilmarkivADT {
+	private int antall;
+	private LinearNode_trur_ikkje_bruk_for<Film> start;
+	
+	
+}
