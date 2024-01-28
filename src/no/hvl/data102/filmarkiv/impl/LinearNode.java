69f9637e7ac83eec0c528e1fb539463c537b082e
672640 <672640@stud.hvl.no>
2024-01-28 17:01:42 +0100

    Endringar

--- src/no/hvl/data102/filmarkiv/impl/LinearNode.java
+++ src/no/hvl/data102/filmarkiv/impl/LinearNode.java
@@ -1,6 +1,6 @@
 package no.hvl.data102.filmarkiv.impl;
-
 public class LinearNode<T> {
 	public T data;
 	public LinearNode<T> neste;
+	
 }
--- src/no/hvl/data102/filmarkiv/impl/Sjanger.java
+++ src/no/hvl/data102/filmarkiv/impl/Sjanger.java
@@ -1,9 +1,5 @@
 package no.hvl.data102.filmarkiv.impl;
 
 public enum Sjanger {
-<<<<<<< HEAD
-	HORROR, KRIM, DRAMA
-=======
 HORROR, KRIM, DRAMA;
->>>>>>> parent of 74ac2e3 (Endingar)
 }
